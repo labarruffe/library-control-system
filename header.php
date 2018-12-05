2824ddb9c32b0f378d9da3d01fbc6b30acb0f488
LABarruffe <lucas_barruffe@hotmail.com>
2018-12-04 23:51:29 -0200

    adicionado favicon na aba do browser

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <link rel="icon" type="image/x-icon" href="favicon-biblioteca.ico" />
   <title>Biblioteca Barrufino</title>
 
   <!-- CSS  -->
@@ -19,6 +20,7 @@
 
 </head>
 <body>
+
   <nav class="light-blue lighten-1" role="navigation">
     <div class="nav-wrapper container">
       <a id="logo-container" href="index.php" class="brand-logo">
