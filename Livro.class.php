ee272b010099afc49238537aeec6e36b5eed07e8
LABarruffe <lucas_barruffe@hotmail.com>
2018-12-04 23:56:51 -0200

    add livro class

---
+++ Livro.class.php
@@ -0,0 +1,80 @@
+<?php
+include "Mysql.class.php";
+
+class Livro {
+    private $id;
+    private $id_fornecedor;
+    private $titulo;
+    private $ano_publicacao;
+    private $edicao;
+    private $editora;
+
+    function __construct($nome, $titulo, $ano_publicacao, $edicao, $editora) {
+        $this->nome=$nome;
+        $this->nome=$titulo;
+        $this->nome=$ano_publicacao;
+        $this->nome=$edicao;
+        $this->nome=$editora;
+    }
+
+    // "método mágico" para criação de set genérico
+	// ou seja, cria um set que pode ser usado por todos os atributos
+	function __set($prop, $val) {
+		$this->$prop = $val;
+    }
+    
+	// "método mágico" para criação de get genérico
+	// ou seja, cria um get que pode ser usado por todos os atributos
+	function __get($prop) {
+		return $this->$prop;
+    }
+    
+    function __toString() {
+        return "<br> Método toString de Livro <br>";
+    }
+
+    function getAll($isOrdered = false) {
+        $mysql = new Mysql();
+        $ok = $mysql->dbConnect() or die ("Não é possível conectar-se ao servidor.");
+
+        if ($isOrdered) {
+            $resultado = mysqli_query($ok, "Select * from livros order by nome") or 
+            die ("Não é possível consultar livros.");
+        } else {
+            $resultado = mysqli_query($ok, "Select * from livros") or 
+            die ("Não é possível consultar livros.");
+        }
+        
+        return $resultado;
+    }
+
+    // function getById($id) {
+    //     $mysql = new Mysql();
+    //     $ok = $mysql->dbConnect() or die ("Não é possível conectar-se ao servidor.");
+        
+    //     $resultado = mysqli_query($ok, "Select * from funcionarios where id = '$id'") or 
+    //     die ("Não é possível retornar dados do funcionário!");
+
+    //     return $resultado;
+    // }
+
+    // function update($id, $nome, $data_contratacao) {
+    //     $mysql = new Mysql();
+    //     $ok = $mysql->dbConnect() or die ("Não é possível conectar-se ao servidor.");
+        
+    //     mysqli_query($ok, "update funcionarios set nome='$nome', data_contratacao='$data_contratacao' where id='$id'") or 
+    //     die ("Não é possível alterar dados do funcionário!");
+    // }
+
+    // function delete($id) {
+    //     $mysql = new Mysql();
+    //     $ok = $mysql->dbConnect() or die ("Não é possível conectar-se ao servidor.");
+
+    //     mysqli_query($ok, "delete from funcionarios where id='$id'") or die ("Não é possível deletar funcionário!");
+    //     print("Funcionário deletado com sucesso (id): $id");
+    // }
+
+
+}
+
+?>
---
+++ lista_livros.php
@@ -0,0 +1,57 @@
+<?php
+    include "header.php";
+
+    include "Livro.class.php";
+
+    // $resultado = Livros::getAll();
+
+    print('
+    
+    ');
+
+    // print('<div class="container">
+    //     <div class="col s12 m8 offset-m1 xl7 offset-xl1">
+    //     <div id="highlight" class="section scrollspy">
+    //     <h3 class="header">Funcionários</h3>
+    //     <div class="row">
+    //     <div class="col s12"> 
+    //         <table class="highlight">
+    //         <thead>
+    //             <tr>
+    //                 <th>Código</th>
+    //                 <th>Nome</th>
+    //                 <th>Data Contratação</th>
+    //                 <th>Deletar</th>
+    //                 <th>Alterar</th>
+    //             </tr>
+    //         </thead>
+    //         <tbody>
+    // ');    
+    
+    // while ($linha=mysqli_fetch_array($resultado)) {
+    //     $Id=$linha["id"];
+    //     $Nome=$linha["nome"];
+    //     $Data_Contratacao=$linha["data_contratacao"];
+    //     $Data_Contratacao=date("d-m-Y", strtotime($Data_Contratacao));
+    //     print("
+    //         <tr>
+    //             <td>$Id</td>
+    //             <td>$Nome</td>
+    //             <td>$Data_Contratacao</td>
+    //             <td><a href='delete_funcionario.php?id=$Id&nome=$Nome&dt_contrat=$Data_Contratacao'>Deletar</a></td>
+    //             <td><a href='update_funcionario.php?id=$Id'>Alterar</a></td>
+    //         </tr>
+    //     ");  
+    // }
+    // print('</tbody>
+    //         </table>
+    //         </div>
+    //         </div>
+    //         </div>
+    //         </div>
+    //         </div>
+    //     ');
+        
+    include "footer.php";
+
+?>
